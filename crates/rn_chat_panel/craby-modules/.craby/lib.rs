1ba33f56f8aa4e994a1ba62118f2379a5975be91
Madeline <maddythewisp@gmail.com>
2026-01-01 07:41:06 -0700

    remove .craby

--- crates/rn_chat_panel/craby-modules/.craby/lib.rs
+++
@@ -1,9 +0,0 @@
-#[rustfmt::skip]
-pub(crate) mod ffi;
-pub(crate) mod generated;
-
-pub(crate) mod zed_icons_impl;
-pub(crate) mod zed_llm_impl;
-pub(crate) mod zed_markdown_impl;
-pub(crate) mod zed_theme_impl;
-pub(crate) mod zed_workspace_impl;
